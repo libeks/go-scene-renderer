8a603d56374b637c9b57f61f23b3888bae9ec134
janis-libeks <janis.libeks@reddit.com>
2024-03-16 18:27:22 -0400

    Move scenes to their own file, remove debug statements

--- main.go
+++ main.go
@@ -8,71 +8,13 @@ import (
 	"path/filepath"
 	"runtime/pprof"
 
-	"github.com/libeks/go-scene-renderer/color"
 	"github.com/libeks/go-scene-renderer/renderer"
-	"github.com/libeks/go-scene-renderer/scenes"
 )
 
 const (
 	PNG_FORMAT = "png"
 	MP4_FORMAT = "mp4"
 	do_pprof   = false
-)
-
-var (
-	// gradient = color.LinearGradient{
-	// 	Points: []color.Color{
-	// 		color.Hex("#6CB4F5"),
-	// 		color.Hex("#EBF56C"),
-	// 		color.Hex("#F5736C"),
-	// 	},
-	// }
-	// gradient = color.LinearGradient{
-	// 	Points: []color.Color{
-	// 		color.Hex("#F590C1"), // pink
-	// 		color.Hex("#000"),
-	// 		color.Hex("#90E8F5"), // light blue
-	// 		color.Hex("#000"),
-	// 		color.Hex("#F590C1"), // pink
-	// 	},
-	// }
-
-	// gradient = color.LinearGradient{
-	// 	Points: []color.Color{
-	// 		color.Hex("#FFF"), // black
-	// 		color.Hex("#DDF522"),
-	// 		color.Hex("#A0514C"),
-	// 		color.Hex("#000"), // white
-	// 	},
-	// }
-	// scene = scenes.SineWaveWCross{
-	// 	XYRatio:      0.0001,
-	// 	SigmoidRatio: 2.0,
-	// 	SinCycles:    3,
-	// 	TScale:       0.3,
-	// 	// TOffset:      0.0,
-	// 	// Gradient:     color.Grayscale,
-	// 	Gradient: gradient,
-	// }
-	// scene = scenes.DummySpinningCube(scenes.Uniform{color.Black})
-	scene = scenes.DummySpinningCube(scenes.SineWave{
-		XYRatio:      0.1,
-		SigmoidRatio: 2,
-		SinCycles:    3,
-		Gradient:     color.Grayscale,
-	})
-	// scene = scenes.DummyTriangle()
-
-	// scene = scenes.HorizGradient{
-	// 	Gradient: gradient,
-	// }
-
-	// scene = scenes.SineWave{
-	// 	XYRatio:      0.1,
-	// 	SigmoidRatio: 2,
-	// 	SinCycles:    3,
-	// 	Gradient:     color.Grayscale,
-	// }
 )
 
 func main() {
@@ -92,10 +34,7 @@ func main() {
 	var videoFlag = flag.String("video", "default", "video options, either <width>,<height>,<interpolate>,<nframes>,<frameRate> or one of default/test/intermediate/hidef")
 
 	flag.Parse()
-	fmt.Printf("image flag: %s\n", *imageFlag)
-	fmt.Printf("video flag: %s\n", *videoFlag)
 	argsWithoutProg := flag.Args()
-	fmt.Printf("Args: %+v\n", argsWithoutProg)
 	if len(argsWithoutProg) != 2 {
 		log.Fatal("Insufficient arguments, expect <type> <output.gif>.")
 	}
---
+++ scene.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"github.com/libeks/go-scene-renderer/color"
+	"github.com/libeks/go-scene-renderer/scenes"
+)
+
+var (
+	// gradient = color.LinearGradient{
+	// 	Points: []color.Color{
+	// 		color.Hex("#6CB4F5"),
+	// 		color.Hex("#EBF56C"),
+	// 		color.Hex("#F5736C"),
+	// 	},
+	// }
+	// gradient = color.LinearGradient{
+	// 	Points: []color.Color{
+	// 		color.Hex("#F590C1"), // pink
+	// 		color.Hex("#000"),
+	// 		color.Hex("#90E8F5"), // light blue
+	// 		color.Hex("#000"),
+	// 		color.Hex("#F590C1"), // pink
+	// 	},
+	// }
+
+	// gradient = color.LinearGradient{
+	// 	Points: []color.Color{
+	// 		color.Hex("#FFF"), // black
+	// 		color.Hex("#DDF522"),
+	// 		color.Hex("#A0514C"),
+	// 		color.Hex("#000"), // white
+	// 	},
+	// }
+	// scene = scenes.SineWaveWCross{
+	// 	XYRatio:      0.0001,
+	// 	SigmoidRatio: 2.0,
+	// 	SinCycles:    3,
+	// 	TScale:       0.3,
+	// 	// TOffset:      0.0,
+	// 	// Gradient:     color.Grayscale,
+	// 	Gradient: gradient,
+	// }
+	// scene = scenes.DummySpinningCube(scenes.Uniform{color.Black})
+	scene = scenes.DummySpinningCube(scenes.SineWave{
+		XYRatio:      0.1,
+		SigmoidRatio: 2,
+		SinCycles:    3,
+		Gradient:     color.Grayscale,
+	})
+	// scene = scenes.DummyTriangle()
+
+	// scene = scenes.HorizGradient{
+	// 	Gradient: gradient,
+	// }
+
+	// scene = scenes.SineWave{
+	// 	XYRatio:      0.1,
+	// 	SigmoidRatio: 2,
+	// 	SinCycles:    3,
+	// 	Gradient:     color.Grayscale,
+	// }
+)
